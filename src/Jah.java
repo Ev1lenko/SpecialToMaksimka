742786aad03e590d053d92037160ed10cff3434c
Sergey Osipenko <ay-q@list.ru>
2014-10-07 16:36:19 +0300

    Olosha

---
+++ src/Jah.java
@@ -0,0 +1,22 @@
+public class Jah {
+	public static void main(String args[]){
+		Reader m = new marihuana();
+		m.Scan();
+		m.i = count(m.i);
+
+			if (m.i=2){
+				System.out.println("Давай не палимся, по тихому навалямися!");
+			}
+			else 
+				{ if (m.i=1){
+				System.out.println("А у меня есть, давай не палимся, по тихому навалямися!");
+						}
+				else {
+					System.out.println("Че ты палишься?");
+				}}	
+	}
+	private static int count(int x){
+		x=x+1;
+		return x;
+	}
+}
---
+++ src/marihuana.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+	public class marihuana {
+		int i;
+		public void Scan(){
+			System.out.println("Макс, есть дед? Если да жми - 1 если нет - 0");
+			Scanner scn = new Scanner(System.in);
+			i = scn.nextInt();
+	}
+
+}
